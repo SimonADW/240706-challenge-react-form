42ee511bff4cd52e3daca9be90551703bcdc0579
Simon A.D. Winter <112318684+SimonADW@users.noreply.github.com>
2024-07-06 19:14:50 +0200

    Add react-form-hook and validation

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,54 +1,99 @@
-import style from "./ContactForm.module.css"
+import { useCallback } from "react";
+import style from "./ContactForm.module.css";
+import { useForm } from "react-hook-form";
 
 const ContactForm = () => {
-  return (
-	<>
-		<form className={style.contactForm} action="">
-			<h1>Contact us</h1>
-			<div className={style.nameContainer}>
-				<div className={style.inputLabelPair}>
-					<label htmlFor="firstName">First Name<sup>*</sup></label>
-					<input type="text" />
-				</div>
-				<div className={style.inputLabelPair}>
-					<label htmlFor="lastName">Last Name <sup>*</sup></label>
-					<input type="text" />
-				</div>
-			</div>
+	const { register, handleSubmit, formState: {errors}} = useForm({
+		defaultValues: {
+			firstName: "",
+			lastName: "",
+			email: "",
+			query: "",
+			message: "",
+			consent: false,
+		}
+	})
 
-			<div className={style.inputLabelPair}>
-				<label htmlFor="email">E-mail Adress:</label>
-				<input type="email" />
-			</div>
+	const onSubmit = useCallback((data)=> {
+		console.log(data);
+		}
+	)
 
-			<div>
-				<label className={style.queryMainLabel} htmlFor="query">Query Type <sup>*</sup></label>
-				<div className={`${style.inputLabelPair} ${style.queryContainer}`}>
-					<div className={style.radioAndLabel}>
-						<input type="radio" name="query" />
-						<label htmlFor="generalEnquery">General Enquiry</label>
+	return (
+		<>
+			<form onSubmit={handleSubmit(onSubmit)} className={style.contactForm} action="">
+				<h1>Contact us</h1>
+				<div className={style.nameContainer}>
+					<div className={style.inputLabelPair}>
+						<label htmlFor="firstName">
+							First Name<sup>*</sup>
+						</label>
+						<input {...register("firstName", { required: "First name is required"})} type="text" />
+						<p>{errors.firstName?.message}</p>
 					</div>
-					<div className={style.radioAndLabel}>
-						<input type="radio" name="query" />
-						<label htmlFor="supportRequest">Support Request</label>
+					<div className={style.inputLabelPair}>
+						<label htmlFor="lastName">
+							Last Name <sup>*</sup>
+						</label>
+						<input {...register("lastName", { required: "Last name is required"})} type="text" />
+						<p>{errors.lastName?.message}</p>
 					</div>
 				</div>
-			</div>
 
-			<div className={style.inputLabelPair}>
-				<label htmlFor="message">Message <sup>*</sup></label>
-				<textarea name="" id="message" rows={6}></textarea>
-			</div>
+				<div className={style.inputLabelPair}>
+					<label htmlFor="email">E-mail Adress:</label>
+					<input {...register("email", {
+						required: {
+							value: true,
+							message: "Email is required", 
+						},
+						pattern: "Please enter a valid e-mail adress",
+					})} type="email" />
+					<p>{errors.email?.message}</p>
+				</div>
 
-			<div className={style.checkboxContainer}>
-				<input type="checkbox" />
-				<label htmlFor="consent">I consent to being contacted by the team <sup>*</sup></label>
-			</div>
+				<div>
+					<label className={style.queryMainLabel} htmlFor="query">
+						Query Type <sup>*</sup>
+					</label>
+					<div
+						className={`${style.inputLabelPair} ${style.queryContainer}`}
+					>
+						<div className={style.radioAndLabel}>
+							<input {...register("query")} type="radio" value="general" id="generalEnquery" checked />
+							<label htmlFor="generalEnquery">
+								General Enquiry
+							</label>
+						</div>
+						<div className={style.radioAndLabel}>
+							<input {...register("query")} type="radio" value="support" id="supportRequest"/>
+							<label htmlFor="supportRequest">
+								Support Request
+							</label>
+						</div>
+					</div>
+				</div>
 
-			<button>Submit</button>
-		</form>
-	</>
-  )
-}
+				<div className={style.inputLabelPair}>
+					<label htmlFor="message">
+						Message <sup>*</sup>
+					</label>
+					<textarea {...register("message", {required: "Please enter a message"})} id="message" rows={6}></textarea>
+					<p>{errors.message?.message}</p>
+				</div>
 
-export default ContactForm
+				<div className={style.checkboxContainer}>
+					<input {...register("consent", { required: "Please agree to being contacted"})} type="checkbox" />
+					<label htmlFor="consent">
+						I consent to being contacted by the team <sup>*</sup>
+					</label>
+				</div>
+				<p>{errors.consent?.message}</p>
+
+				<button>Submit</button>
+			</form>
+		</>
+	);
+};
+
+export default ContactForm;
