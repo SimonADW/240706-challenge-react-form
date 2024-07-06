f1e8ddf2a26eb6378b12ef8b1f185a6711bcdb54
Simon A.D. Winter <112318684+SimonADW@users.noreply.github.com>
2024-07-06 17:51:49 +0200

    Lay out form for mobile

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -6,40 +6,40 @@ const ContactForm = () => {
 	<>
 		<form className={style.contactForm} action="">
 			<h1>Contact us</h1>
-			<div className={style.inputTwoColumn}>
-				<div>
+			<div className={style.nameContainer}>
+				<div className={style.inputLabelPair}>
 					<label htmlFor="firstName">First Name<sup>*</sup></label>
 					<input type="text" />
 				</div>
-				<div>
+				<div className={style.inputLabelPair}>
 					<label htmlFor="lastName">Last Name <sup>*</sup></label>
 					<input type="text" />
 				</div>
 			</div>
 
-			<div className={style.inputOneColumn}>
+			<div className={style.inputLabelPair}>
 				<label htmlFor="email">E-mail Adress:</label>
 				<input type="email" />
 			</div>
 
+			<div className={`${style.inputLabelPair} ${style.queryContainer}`}>
 			<label htmlFor="query">Query Type <sup>*</sup></label>
-			<div>
-				<div>
-					<input type="radio" />
+				<div className={style.radioAndLabel}>
+					<input type="radio" name="query" />
 					<label htmlFor="generalEnquery">General Enquiry</label>
 				</div>
-				<div>
-					<input type="radio" />
+				<div className={style.radioAndLabel}>
+					<input type="radio" name="query" />
 					<label htmlFor="supportRequest">Support Request</label>
 				</div>
 			</div>
 
-			<div className={style.inputOneColumn}>
+			<div className={style.inputLabelPair}>
 				<label htmlFor="message">Message <sup>*</sup></label>
-				<textarea name="" id="message"></textarea>
+				<textarea name="" id="message" rows={6}></textarea>
 			</div>
 
-			<div>
+			<div className={style.checkboxContainer}>
 				<input type="checkbox" />
 				<label htmlFor="consent">I consent to being contacted by the team <sup>*</sup></label>
 			</div>
